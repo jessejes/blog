240c07e6ee51ba6988c514d4a4342b0e56332ac7
jesse <92591280+jessejes@users.noreply.github.com>
2023-03-08 21:59:15 +0100

    🎨 Blog page has now a style

--- src/pages/blogs/[slug].tsx
+++ src/pages/blogs/[slug].tsx
@@ -1,7 +1,8 @@
 import Blog from "@/types/blog";
 import { loadBlogsFromDirectory, loadSpecificBlog } from "@/utils/content";
-import { GetStaticProps } from "next";
 import md from 'markdown-it';
+import Link from "next/link";
+import styles from "@/styles/BlogPage.module.scss"
 
 interface BlogPageProps {
     blog: Blog;
@@ -26,9 +27,16 @@ export async function getStaticProps({ params: { slug }}) {
 export default function BlogPage(props: BlogPageProps) {
 
     return (
-        <div>
-            <h1>{props.blog.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: md().render(props.blog.content) }} />
+        <div className={styles.blog}>
+            <nav className={styles.breadcrumb}>
+                <Link href="/">Home</Link>
+                <div>»</div>
+                <Link href="/blogs">Blogs</Link>
+            </nav>
+            <div className={styles.content}>
+                <h1>{props.blog.title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: md().render(props.blog.content) }} />
+            </div>
         </div>
     )
 }
